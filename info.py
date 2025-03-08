17199264377403bf445ce8cf4b0bd56fdaca05d6
Swar73 <swaryadav18@gmail.com>
2025-03-08 14:19:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Swar73:Sawan7317@@cluster0.lrroe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-DATABASE_NAME = environ.get('DATABASE_NAME', "swar73")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
